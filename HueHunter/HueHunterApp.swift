3e87efce3d0277581ae9966b7bc3c227ae0bc602
NouraSultan <noura6sultan@gmail.com>
2023-01-12 15:11:10 +0300

    Initial Commit

---
+++ HueHunter/HueHunterApp.swift
@@ -0,0 +1,17 @@
+//
+//  HueHunterApp.swift
+//  HueHunter
+//
+//  Created by Noura Sultan Ibn Qurmulah on 19/06/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct HueHunterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
